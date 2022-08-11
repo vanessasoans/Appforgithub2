b639d4d7a79f4bba124b7155c10882a97dcab67d
vanessasoans <vanessasharon007@gmail.com>
2022-08-11 10:51:05 +0530

    New line added

--- Appforgithub2/Program.cs
+++ Appforgithub2/Program.cs
@@ -7,6 +7,7 @@ namespace Appforgithub2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Today is Thursday");
         }
     }
 }
